1428038d944186a4192adf11b48ad9c9af7db83e
gaeqs <gaeqs@users.noreply.github.com>
2021-02-22 21:27:20 +0000

    deploy: 4964c6b2e94581305625c674e3d2298d5cbe65b1

---
+++ js/jyd_demo.js
@@ -0,0 +1,33 @@
+document.getElementById("demo_input_button").onclick = function (event) {
+
+  let video = document.getElementById("demo_input").value;
+  let output = document.getElementById("demo_output");
+
+  output.innerText = "Loading...";
+
+  const request = new XMLHttpRequest();
+  const url = "http://80.241.215.252:46155/demo?url=".concat(video);
+  request.open("GET", url, true);
+
+  request.onload = function (pEvent) {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        parse(request.responseText, output);
+      } else {
+        output.innerText = "Error! " + request.status;
+      }
+    }
+  }
+
+  request.send();
+}
+
+function parse(string, element) {
+  element.innerHTML = "<h4>Protected videos may not work in your explorer: they can only be accessed by the " +
+    "machine sending the request to the YouTube server!</h4>";
+  let options = string.split('\n')
+  options.forEach(function (value, index, array) {
+    let data = value.split('\t');
+    element.innerHTML += "<a href='" + data[1] + "'> <h5>" + data[0] + "</h5> </a>";
+  });
+}
