f5096c695a1fc610c4bc324489b60141432955c0
Gael Rial Costas <gael.rial.costas@gmail.com>
2021-10-15 20:11:47 +0200

    Update JYD demo URL.

--- functions/ga.js
+++ functions/ga.js
@@ -7,7 +7,7 @@ const GA_ENDPOINT = `https://www.google-analytics.com/collect`;
 const originallowlist = []; // keep this empty and append domains to allowlist using allowlistDomain()
 // Update me.
 allowlistDomain("gaeqs.github.io/");
-allowlistDomain("80.241.215.252:46155/");
+allowlistDomain("gaeqs.webredirect.org:8890/");
 
 function allowlistDomain(domain, addWww = true) {
   const prefixes = ["https://", "http://"];
